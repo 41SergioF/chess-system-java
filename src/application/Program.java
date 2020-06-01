2158c786a0c702ee72cc15790fdf5bcdc53cf39b
CarlosSergio <carlossergio2401@outlook.com>
2020-06-01 19:55:51 -0300

    First class: Positio

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		
-		System.out.println("knv");
-		
+
+		Position p = new Position(5,5);
+		System.out.println(p);
 	}
 
 }
